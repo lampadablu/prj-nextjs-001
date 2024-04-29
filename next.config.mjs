bafc11500eecfafb7d797019cf71a0d90e5b1eae
Lampadablu <lampadablu@gmail.com>
2024-04-29 17:21:19 +0200

    update deploy

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     output: "export",  // <=== enables static exports
     distDir: 'build',
     reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
 };
 
 export default nextConfig;
