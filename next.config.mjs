1279298d3e657e3d20101d764b9cbbbfa081493f
Lampadablu <lampadablu@gmail.com>
2024-04-29 17:00:39 +0200

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/prj-nextjs-001",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 export default nextConfig;
