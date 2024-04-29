4c99cb304b362ea1816b379dc92245918dea719c
Lampadablu <lampadablu@gmail.com>
2024-04-29 17:25:38 +0200

    update build dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/prj-nextjs-001",
-    output: "export",  // <=== enables static exports
-    distDir: 'build',
+    output: "export",
     reactStrictMode: true,
     images: {
       unoptimized: true,
